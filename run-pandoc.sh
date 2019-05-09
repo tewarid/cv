afcff65556d330e204c866fa157c167375f8588f
Devendra Tewari <devendra.tewari@gmail.com>
2019-05-09 10:43:22 -0300

    pandoc-latex-template as git submodule

--- run-pandoc.sh
+++ run-pandoc.sh
@@ -1,2 +1,2 @@
-pandoc -f markdown title.md cv.md -o cv-letter.pdf -N --template ./eisvogel.tex -V titlepage -V papersize=letter -V caption-justification=centering
-pandoc -f markdown title.md cv.md -o cv-a4.pdf -N --template ./eisvogel.tex -V titlepage -V papersize=a4 -V caption-justification=centering
+pandoc -f markdown title.md cv.md -o cv-letter.pdf -N --template ./pandoc-latex-template/eisvogel.tex -V titlepage -V papersize=letter -V caption-justification=centering
+pandoc -f markdown title.md cv.md -o cv-a4.pdf -N --template ./pandoc-latex-template/eisvogel.tex -V titlepage -V papersize=a4 -V caption-justification=centering
--- title.md
+++ title.md
@@ -1,6 +1,6 @@
 ---
 title: "Devendra Kumar Tewari"
 subtitle: "Curriculum Vitae"
-date: 2019-23-01
+date: 2019-05-09
 tags: [cv, c#, java, ios, android, git, github]
 ...
